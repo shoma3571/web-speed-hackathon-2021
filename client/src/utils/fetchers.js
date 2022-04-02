c6f59d32a130c4b50e1ee80ba6517bc658c608c9
Shoma Kobayashi <sho1228.k@gmail.com>
2022-04-02 14:49:34 +0900

    jqueryの削除

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -1,18 +1,17 @@
 import { gzip } from 'pako';
 
+const createFetchError = res => {
+  return new Error(`Failed to fetch(${res.status}): ${res.statusText}`)
+}
+
 /**
  * @param {string} url
  * @returns {Promise<ArrayBuffer>}
  */
 async function fetchBinary(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'binary',
-    method: 'GET',
-    responseType: 'arraybuffer',
-    url,
-  });
-  return result;
+  const res = await fetch(url)
+  if (!res.ok) throw createFetchError(res)
+  return res.arrayBuffer()
 }
 
 /**
@@ -21,13 +20,9 @@ async function fetchBinary(url) {
  * @returns {Promise<T>}
  */
 async function fetchJSON(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'json',
-    method: 'GET',
-    url,
-  });
-  return result;
+  const res = await fetch(url)
+  if (!res.ok) throw createFetchError(res)
+  return res.json()
 }
 
 /**
@@ -37,18 +32,15 @@ async function fetchJSON(url) {
  * @returns {Promise<T>}
  */
 async function sendFile(url, file) {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: 'json',
-    headers: {
-      'Content-Type': 'application/octet-stream',
-    },
+  const res = await fetch(url, {
     method: 'POST',
-    processData: false,
-    url,
-  });
-  return result;
+    body: file,
+    headers: {
+      'Content-Type': 'application/octet-stream'
+    }
+  })
+  if (!res.ok) throw createFetchError(res)
+  return res.json()
 }
 
 /**
@@ -62,19 +54,16 @@ async function sendJSON(url, data) {
   const uint8Array = new TextEncoder().encode(jsonString);
   const compressed = gzip(uint8Array);
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: 'json',
+  const res = await fetch(url, {
+    method: 'POST',
+    body: compressed.buffer,
     headers: {
       'Content-Encoding': 'gzip',
       'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    processData: false,
-    url,
-  });
-  return result;
+    }
+  })
+  if (!res.ok) throw createFetchError(res)
+  return res.json()
 }
 
 export { fetchBinary, fetchJSON, sendFile, sendJSON };
