0683a0c637d1c6569556b61c94b56cf75e64eaa8
Shoma Kobayashi <sho1228.k@gmail.com>
2022-04-03 10:38:55 +0900

    最新のchromeのみに対応するように変更

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,8 +23,6 @@ const config = {
   },
   entry: {
     main: [
-      'core-js',
-      'regenerator-runtime/runtime',
       path.resolve(SRC_PATH, './index.css'),
       path.resolve(SRC_PATH, './buildinfo.js'),
       path.resolve(SRC_PATH, './index.jsx'),
